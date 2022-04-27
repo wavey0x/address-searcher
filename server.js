9db3596b15d80583b382aa52443f4d6bed07f501
wavey0x <retrowaveyy@gmail.com>
2022-04-27 17:35:42 -0400

    feat: cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express()
 
 let port = 3001
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));;
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin');
     res.header(
